f01a9d1a4189a27f6e83e7f47a6e9170e4f78db8
Pawel Gedlek <pawgedlek@gmail.com>
2025-02-18 19:33:47 +0100

    Prepare backbone for product page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import './App.css'
 
-import { FaGithub } from 'react-icons/fa';
+import Products from './components/Products';
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline font-montserrat shadow-custom text-banner-color4 custom-gradient2">
-        <span><FaGithub /></span> Setup tailwind CSS!
-      </h1>
+      <Products />
     </>
   )
 }

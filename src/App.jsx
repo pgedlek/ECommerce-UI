4b8da0f6234842098e80ddbdabedd804bfe6d57a
Pawel Gedlek <pawgedlek@gmail.com>
2025-02-18 18:59:51 +0100

    Add Tailwind CSS and MaterialUI into project

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ function App() {
 
   return (
     <>
-      <p>
-        Setup
-      </p>
+      <h1 className="text-3xl font-bold underline font-montserrat shadow-custom text-banner-color4 custom-gradient2">
+        Setup tailwind CSS!
+      </h1>
     </>
   )
 }

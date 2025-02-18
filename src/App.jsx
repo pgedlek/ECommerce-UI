18db261c9ab9478ec78d7dfd67f11c2e85e6ed56
Pawel Gedlek <pawgedlek@gmail.com>
2025-02-18 19:05:53 +0100

    Add react icons to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 
+import { FaGithub } from 'react-icons/fa';
+
 function App() {
 
   return (
     <>
       <h1 className="text-3xl font-bold underline font-montserrat shadow-custom text-banner-color4 custom-gradient2">
-        Setup tailwind CSS!
+        <span><FaGithub /></span> Setup tailwind CSS!
       </h1>
     </>
   )

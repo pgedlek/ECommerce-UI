6d23852dc202207f79b63fe4ba8d0f6f8d523d58
Pawel Gedlek <pawgedlek@gmail.com>
2025-02-19 22:08:54 +0100

    Implement fetching product from backend using redux

---
+++ src/store/reducers/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { productReducer } from "./productReducer";
+
+export const store = configureStore({
+    reducer: {
+        products: productReducer,
+    },
+    preloadedState: {}
+});
+
+export default store;

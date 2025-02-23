19c113ca5ca6fb8b5363f9afaee284afde446923
Pawel Gedlek <pawgedlek@gmail.com>
2025-02-23 20:15:15 +0100

    Add Navbar with cart icon and loginbutton

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/home/Home';
 import Products from './components/products/Products';
+import Navbar from './components/shared/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path='/products' element={<Products />} />

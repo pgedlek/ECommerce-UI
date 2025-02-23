51cd2b724f0db21a3ae85455ad21dd52a49c044e
Pawel Gedlek <pawgedlek@gmail.com>
2025-02-23 20:37:35 +0100

    Build the About page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/home/Home';
 import Products from './components/products/Products';
 import Navbar from './components/shared/Navbar';
+import About from './components/about/About';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path='/products' element={<Products />} />
+        <Route path='/about' element={<About />} />
       </Routes>
     </BrowserRouter>
   );

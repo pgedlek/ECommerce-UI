d06032c7159e07475452950576e5c4434f222c3c
Pawel Gedlek <pawgedlek@gmail.com>
2025-02-26 19:08:27 +0100

    Setup auth reducer

--- src/store/reducers/store.js
+++ src/store/reducers/store.js
@@ -2,13 +2,19 @@ import { configureStore } from "@reduxjs/toolkit";
 import { productReducer } from "./productReducer";
 import { errorReducer } from "./errorReducer";
 import { cartReducer } from "./cartReducer";
+import { authReducer } from "./authReducer";
 
 const cartItems = localStorage.getItem("cartItems") && localStorage.getItem("cartItems") !== "undefined"
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
 
+const user = localStorage.getItem("auth") && localStorage.getItem("auth") !== "undefined"
+  ? JSON.parse(localStorage.getItem("auth"))
+  : [];
+
 const initialState = {
-  carts: { cart: cartItems }
+  auth: { user: user },
+  carts: { cart: cartItems },
 };
 
 export const store = configureStore({
@@ -16,6 +22,7 @@ export const store = configureStore({
     products: productReducer,
     errors: errorReducer,
     carts: cartReducer,
+    auth: authReducer,
   },
   preloadedState: initialState
 });
